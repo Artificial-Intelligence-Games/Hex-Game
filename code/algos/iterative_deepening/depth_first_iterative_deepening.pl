343cd9a699687a8f9a58b242e09db56716dbf9ec
Ricardo Bernardino <76744984+Ricardo-Bernardino-dev@users.noreply.github.com>
2023-09-14 15:35:40 +0100

    Deleted wrong code files

--- code/algos/iterative_deepening/depth_first_iterative_deepening.pl
+++
@@ -1,8 +0,0 @@
-:- consult('path.pl').
-
-depth_first_iterative_deepening( Node, Solution) :-
-	path( Node, GoalNode, Solution),
-	goal( GoalNode).
-
-
-% ?- depth_first_iterative_deepening(a, Solution).
--- code/algos/iterative_deepening/path.pl
+++
@@ -1,9 +0,0 @@
-path(Node, Node, [Node]). % Single node path
-
-path(FirstNode, LastNode, [LastNode | Path]) :-
-	path(FirstNode, OneButLast, Path), % Path up to one-but-last node
-	s(OneButLast, LastNode),           % Last step
-	\+ member(LastNode, Path).         % No cycle
-
-
-% ?- path(a, Last, Path).
